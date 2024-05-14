fb63abfc6f36bb0c8f657a0f98d71aed04046c09
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2024-05-14 19:00:53 +0300

    rebuild, update, local chat window

--- main.ts
+++ main.ts
@@ -5,8 +5,10 @@ import {
 	Notice,
 	Plugin,
 	PluginSettingTab,
+	WorkspaceLeaf,
 	Setting,
 	requestUrl,
+	ItemView,
 } from "obsidian";
 
 interface llmSettings {
@@ -19,11 +21,166 @@ const DEFAULT_SETTINGS: llmSettings = {
 	model: "tinyllama",
 };
 
+export const VIEW_TYPE_EXAMPLE = "example-view";
+
+export class ExampleView extends ItemView {
+	settings: llmSettings;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	constructor(leaf: WorkspaceLeaf, settings: any) {
+		super(leaf);
+		this.settings = settings;
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Chat View";
+	}
+
+	async onOpen() {
+		const set = this.settings;
+		async function getChatResponsefromLLM(passedQuery: string) {
+			const llmSettings = set;
+			const llmModel = llmSettings.model;
+			const endpoint = `${llmSettings.ollama_endpoint}`;
+			const inputString = `${passedQuery}`;
+			const bodyOb = {
+				prompt: inputString,
+				model: llmModel,
+				stream: true,
+			};
+
+			try {
+				return await requestUrl({
+					url: endpoint,
+					method: "POST",
+					body: JSON.stringify(bodyOb),
+				}).then((data) => {
+					const res = data.text;
+					if (res) {
+						// eslint-disable-next-line prefer-const
+						let data_list = res;
+						// eslint-disable-next-line prefer-const, @typescript-eslint/no-explicit-any
+						let responses: any[] = [];
+						data_list.split("}").forEach((obj) => {
+							if (obj.trim()) {
+								obj = obj + "}";
+								// eslint-disable-next-line prefer-const
+								let response = JSON.parse(obj)["response"];
+								responses.push(response);
+							}
+						});
+
+						// eslint-disable-next-line prefer-const
+						let response_line = responses.join("");
+
+						// eslint-disable-next-line prefer-const
+						let bot_response = `\n\n${response_line}\n\n`;
+
+						// eslint-disable-next-line prefer-const
+						let response = bot_response;
+						new Notice("Success !");
+						return String(response);
+					}
+				});
+			} catch (error) {
+				new Notice(`Error: ${error}!`);
+				return String(error);
+			}
+		}
+
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("div", {
+			attr: {
+				id: "chat_box",
+				style: "white-space: pre-wrap;font-size:1.2rem;overflow:scroll;width:95%;padding:2.5px;border-radius:8px;height:400px;display:block;margin-top:2rem;margin-bottom:0;border:1px solid white;",
+			},
+		});
+		container.createEl("input", {
+			attr: {
+				id: "input_field",
+				placeholder: "Write your text here",
+				style: "width:95%;padding:4px;border-radius:8px;height:4rem;font-size:1.2rem;display:block;margin-top:2rem;margin-bottom:0;border:1px solid white;",
+			},
+		});
+		container.createEl("button", {
+			text: "Ask local LLM",
+			attr: {
+				id: "submit_button",
+				style: "width:95%;padding:2.5px;border-radius:8px;display:block;font-size:1.2rem;margin-top:1rem;margin-bottom:0;border:1px solid white;",
+			},
+		});
+		container.createEl("button", {
+			text: "Clean chat",
+			attr: {
+				id: "clean_button",
+				style: "width:95%;padding:2.5px;border-radius:8px;display:block;font-size:1.2rem;margin-top:1rem;margin-bottom:0;border:1px solid white;",
+			},
+		});
+		const inputSelector = container.querySelector("#input_field");
+		const buttonSelector = container.querySelector("#submit_button");
+		const chatboxSelector = container.querySelector("#chat_box");
+		const cleanSelector = container.querySelector("#clean_button");
+		let inputvalue: any;
+		const paragraph = document.createElement("p");
+		paragraph.textContent = "Local LLM chat";
+		inputSelector?.addEventListener("keyup", (event) => {
+			const target = event.target as HTMLButtonElement;
+			if (target) {
+				inputvalue = target.value;
+			}
+		});
+
+		buttonSelector?.addEventListener("click", () => {
+			// eslint-disable-next-line prefer-const
+			let passingValue = inputvalue;
+			const waitingForAnswer = `\nWaiting for answer to prompt: ${passingValue}\n`;
+
+			if (chatboxSelector) {
+				chatboxSelector.textContent += waitingForAnswer;
+			}
+			new Notice("Trying to send a request to LLM !\n\nPatience !");
+			getChatResponsefromLLM(passingValue).then((data) => {
+				// eslint-disable-next-line prefer-const
+				let answerBox = String(data);
+				if (chatboxSelector) {
+					chatboxSelector.textContent += `\n${answerBox}\n`;
+				}
+			});
+		});
+		if (cleanSelector) {
+			cleanSelector.addEventListener("click", () => {
+				if (chatboxSelector) {
+					chatboxSelector.textContent = "";
+				}
+			});
+		}
+	}
+	async onClose() {
+		// Nothing to clean up.
+	}
+}
+
 export default class llmPlugin extends Plugin {
 	settings: llmSettings;
-
 	async onload() {
 		const settings = await this.loadSettings();
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf, settings)
+		);
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const ribbonIconEl = this.addRibbonIcon(
+			"star",
+			"Local LLM Chat",
+			(evt: MouseEvent) => {
+				this.activateView();
+			}
+		);
 
 		async function getResponsefromLLM(passedQuery: string) {
 			const llmSettings = settings;
@@ -64,7 +221,7 @@ export default class llmPlugin extends Plugin {
 						let bot_response = `\n\n${response_line}\n\n`;
 
 						// eslint-disable-next-line prefer-const
-						let response = bot_response.trim();
+						let response = bot_response;
 						new Notice("Success !");
 						return String(response);
 					}
@@ -134,6 +291,30 @@ export default class llmPlugin extends Plugin {
 
 	onunload() {}
 
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+		if (leaves.length > 0) {
+			// A leaf with our view already exists, use that
+			leaf = leaves[0];
+		} else {
+			// Our view could not be found in the workspace, create a new leaf
+			// in the right sidebar for it
+			leaf = workspace.getRightLeaf(false);
+			if (leaf) {
+				await leaf.setViewState({
+					type: VIEW_TYPE_EXAMPLE,
+					active: true,
+				});
+				// "Reveal" the leaf in case it is in a collapsed sidebar
+				workspace.revealLeaf(leaf);
+			}
+		}
+	}
+
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
@@ -190,6 +371,7 @@ class llmSettingsTab extends PluginSettingTab {
 						.addOption("tinydolphin", "tinydolphin")
 						.addOption("samantha-mistral", "samantha-mistral")
 						.addOption("llama2", "llama2")
+						.addOption("llama3", "llama3")
 						.addOption("medllama2", "medllama2")
 						.setValue(this.settings.model)
 						.onChange(async (value) => {
@@ -211,6 +393,7 @@ class llmSettingsTab extends PluginSettingTab {
 						.addOption("tinydolphin", "tinydolphin")
 						.addOption("samantha-mistral", "samantha-mistral")
 						.addOption("llama2", "llama2")
+						.addOption("llama3", "llama3")
 						.addOption("medllama2", "medllama2")
 						.setValue("tinyllama")
 						.onChange(async (value) => {
